2c91950e02405dd5478ed2460b247265a65d9daf
clemens.poblotzki <clemens.poblotzki@centigrade.de>
2025-08-07 00:09:10 +0200

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,30 @@
-#include <Arduino.h>
-#include <FastLED.h>
-#include "VL53L0X.h"
+#include <Wire.h>
+#include <Adafruit_VL53L0X.h>
 
-#define PIN_BUTTON 3
-#define PIN_LED    2
-#define NUM_LEDS   1
-
-CRGB leds[NUM_LEDS];
-uint8_t led_ih             = 0;
-uint8_t led_status         = 0;
-String led_status_string[] = {"Rainbow", "Red", "Green", "Blue"};
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println("Stamp-C3 demo!");
+  Serial.begin(115200);
+  Wire.begin(18, 19); // SDA = GPIO8, SCL = GPIO9 (adjust if needed)
 
-    pinMode(PIN_BUTTON, INPUT_PULLUP);
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while (1);
+  }
 
-    FastLED.addLeds<SK6812, PIN_LED, GRB>(leds, NUM_LEDS);
+  Serial.println(F("VL53L0X ready"));
 }
 
 void loop() {
-    switch (led_status) {
-        case 0:
-            leds[0] = CHSV(led_ih, 255, 255);
-            break;
-        case 1:
-            leds[0] = CRGB::Red;
-            break;
-        case 2:
-            leds[0] = CRGB::Green;
-            break;
-        case 3:
-            leds[0] = CRGB::Blue;
-            break;
-        default:
-            break;
-    }
-    FastLED.show();
-    led_ih++;
-    delay(15);
+  VL53L0X_RangingMeasurementData_t measure;
 
-    if (!digitalRead(PIN_BUTTON)) {
-        delay(5);
-        if (!digitalRead(PIN_BUTTON)) {
-            led_status++;
-            if (led_status > 3) led_status = 0;
-            while (!digitalRead(PIN_BUTTON))
-                ;
-            Serial.print("LED status updated: ");
-            Serial.println(led_status_string[led_status]);
-        }
-    }
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println("Out of range");
+  }
+
+  delay(500);
 }
