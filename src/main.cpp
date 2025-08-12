13710d38761d258bed36caccd98d42e6273b48fb
clemens.poblotzki <clemens.poblotzki@centigrade.de>
2025-08-12 22:46:18 +0200

    initial

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,39 @@
 #include <Wire.h>
-#include <Adafruit_VL53L0X.h>
+#include <VL53L0X.h>
 
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+VL53L0X sensor;
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin(18, 19); // SDA = GPIO8, SCL = GPIO9 (adjust if needed)
+  Wire.begin(18, 19); // or your chosen pins
 
-  if (!lox.begin()) {
-    Serial.println(F("Failed to boot VL53L0X"));
+  sensor.setTimeout(500);
+  if (!sensor.init()) {
+    Serial.println("Failed to detect and initialize sensor!");
     while (1);
   }
 
-  Serial.println(F("VL53L0X ready"));
+  // Optional: better short-range stability
+  sensor.setSignalRateLimit(0.1);            // Lower limit = better close range accuracy
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+
+  // Increase timing budget for accuracy (default = 33 ms)
+  sensor.setMeasurementTimingBudget(50000);  // 50 ms
+   sensor.setSignalRateLimit(.25);
+   //sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+//sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
 }
 
 void loop() {
-  VL53L0X_RangingMeasurementData_t measure;
+  uint16_t distance = sensor.readRangeSingleMillimeters();
 
-  lox.rangingTest(&measure, false); // pass in 'true' to get debug data
-
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  if (sensor.timeoutOccurred()) {
+    Serial.print(" TIMEOUT");
   } else {
-    Serial.println("Out of range");
+    Serial.print("Distance (mm): ");
+    Serial.println(distance);
   }
 
-  delay(500);
+  delay(100);
 }
